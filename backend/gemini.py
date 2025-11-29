ca3b19d10c389a44ffc561fb551bafe1a2a1d5c7
Eryk Zarębski <erykzarebski0@gmail.com>
2025-11-29 13:08:47 +0100

    Switch to google sdk

--- backend/gemini.py
+++ backend/gemini.py
@@ -9,16 +9,36 @@ dotenv.load_dotenv()
 
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-client = OpenAI(
-    api_key=gemini_api_key,
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+# client = OpenAI(
+#     api_key=gemini_api_key,
+#     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+# )
+
+# res = client.chat.completions.create(
+#     model="gemini-2.5-flash",
+#     tools=[{"type": "websearch"}],
+#     messages=[
+#         {"role": "user", "content": "What time is it?"}
+#     ]
+# )
+
+from google import genai
+from google.genai import types
+
+client = genai.Client()
+
+grounding_tool = types.Tool(
+    google_search=types.GoogleSearch()
 )
 
-res = client.chat.completions.create(
+config = types.GenerateContentConfig(
+    tools=[grounding_tool]
+)
+
+res = client.models.generate_content(
     model="gemini-2.5-flash",
-    messages=[
-        {"role": "user", "content": "Write a short poem about the sea."}
-    ]
+    contents="What time is it in Warsaw?",
+    config=config,
 )
 
-print(res)
+print(res.text)
